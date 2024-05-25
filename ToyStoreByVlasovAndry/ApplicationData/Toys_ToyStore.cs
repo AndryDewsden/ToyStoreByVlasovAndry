e11a348856d6efd386623e831b2ec8e3d085cec5
Елена <@DESKTOP-4F1OCS8>
2024-05-25 19:49:28 +0300

    1505

--- ToyStoreByVlasovAndry/ApplicationData/Toys_ToyStore.cs
+++ ToyStoreByVlasovAndry/ApplicationData/Toys_ToyStore.cs
@@ -31,7 +31,20 @@ namespace ToyStoreByVlasovAndry.ApplicationData
         public int toy_id_ageCategory { get; set; }
         public string toy_discription { get; set; }
         public string toy_image { get; set; }
-    
+        public string pic
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(toy_image) || String.IsNullOrWhiteSpace(toy_image))
+                {
+                    return "images\\picture.png";
+                }
+                else
+                {
+                    return "images\\" + toy_image;
+                }
+            }
+        }
         public virtual AgeCategories_ToyStore AgeCategories_ToyStore { get; set; }
         public virtual Categories_ToyStore Categories_ToyStore { get; set; }
         public virtual Countries_ToyStore Countries_ToyStore { get; set; }
